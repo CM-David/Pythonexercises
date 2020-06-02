619e41f9f71830c3355ecebfb353c37a929ed48b
CM-David <CM.David88@yahoo.com>
2020-06-02 17:06:00 -0500

    Figured this one out

---
+++ ex3.py
@@ -0,0 +1,8 @@
+print("Lets madlib, fill in the blanks to the sentence below")
+print("     's favorite movie is     ?")
+
+name = input("What is your name?   ")
+movie = input("What is your favorite movie of all time?   ")
+print(f'Your name is {name.capitalize()}')
+print(f'Favorite movie is {movie}.')
+print(f'{name.capitalize()}\'s all time favorite movie is {movie}.')
