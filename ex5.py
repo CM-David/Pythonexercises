5c1bbe8fa3d6916976f53979817a551649934cce
CM-David <CM.David88@yahoo.com>
2020-06-02 18:48:08 -0500

    If i wouldve read the instructions better this wouldve been faster

---
+++ ex5.py
@@ -0,0 +1,17 @@
+
+day = int(input('Day (0-6)? '))
+if day == 0:
+    print('Sleep in')
+if day == 1:
+    print('Go to work')
+if day == 2:
+    print('Go to work')
+if day == 3:
+    print('Go to work')
+if day == 4:
+    print('Go to work')
+if day == 5:
+    print('Go to work')
+if day == 6:
+    print('Sleep in')
+
